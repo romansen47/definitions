480ecb109ffcf6923f3fd219383f5494c3f1a277
RoManski <romansen47@gmail.com>
2019-05-09 18:59:53 +0200

    subSpaces addition

--- src/main/java/definitions/structures/generic/finitedimensional/defs/subspaces/IFiniteDimensionalSubSpace.java
+++ src/main/java/definitions/structures/generic/finitedimensional/defs/subspaces/IFiniteDimensionalSubSpace.java
@@ -1,8 +1,14 @@
 package definitions.structures.generic.finitedimensional.defs.subspaces;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import definitions.structures.abstr.IVector;
 import definitions.structures.generic.finitedimensional.defs.mappings.IFiniteDimensionalLinearMapping;
 import definitions.structures.generic.finitedimensional.defs.spaces.IFiniteDimensionalVectorSpace;
+import definitions.structures.generic.finitedimensional.defs.vectors.FiniteVector;
+import definitions.structures.generic.finitedimensional.defs.vectors.IFiniteVector;
 
 public interface IFiniteDimensionalSubSpace extends IFiniteDimensionalVectorSpace {
 
@@ -17,4 +23,17 @@ public interface IFiniteDimensionalSubSpace extends IFiniteDimensionalVectorSpac
 	}
 
 	IFiniteDimensionalLinearMapping getParametrization();
+
+	default IVector add(IFiniteVector vec1, IFiniteVector vec2) throws Throwable {
+		if (vec1.getDim() == vec2.getDim() && getSuperSpace().dim() == dim()) {
+			final List<IFiniteVector> base = genericBaseToList();
+			final Map<IFiniteVector, Double> coordinates = new HashMap<>();
+			for (final IFiniteVector vec : base) {
+				coordinates.put(vec, vec1.getCoordinates().get(vec) + vec2.getCoordinates().get(vec));
+			}
+			return new FiniteVector(coordinates);
+		} else {
+			throw new Exception();
+		}
+	}
 }
