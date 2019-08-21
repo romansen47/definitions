ccefc85c440e1f6ac7af98f5ff990ea701f7d154
Ro Mansen <romansen47@gmail.com>
2019-08-21 21:30:21 +0200

    ring elements - splitting here

---
+++ src/main/java/definitions/structures/abstr/vectorspaces/RingElement.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package definitions.structures.abstr.vectorspaces;
+
+import definitions.structures.abstr.groups.GroupElement;
+
+/**
+ * @author RoManski
+ *
+ */
+public interface RingElement extends GroupElement {
+
+}
